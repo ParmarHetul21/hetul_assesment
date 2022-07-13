7a374314ba435391224986aad8820fc5e7d4e436
ParmarHetul21 <hetul.d.parmar.20@gmail.com>
2022-07-13 11:19:20 +0530

    almost compeletd the psd to html with css

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // MAIN JS
+// counter
+const score = document.querySelector(".incremental");
+var counter = 0;
+var interval;
+
+window.onload = () => {
+	interval = setInterval(() => {
+		counter += 5;
+		score.textContent = counter;
+
+		if (counter >= 256) {
+			clearInterval(interval);
+		}
+	}, 100);
+};
